5f2eef97875fe166464597361540f69c6a9ca5ba
dvaknheo <85811616@qq.com>
2021-05-15 13:27:27 +0800

    重新设计了首页

--- view/index.php
+++ view/index.php
@@ -5,76 +5,101 @@
 		<title></title>
 		<link href="/layui/css/layui.css" rel="stylesheet" />
         <style>
-        
-.content {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 100%;
-    text-align: center;
-}
-.content-r>h1 {
-    font-size: 72px;
-    color: #434e59;
-    margin-bottom: 24px;
-    font-weight: 600;
-}
 
-.content-r>p {
-	font-size: 20px;
-	color: rgba(0, 0, 0, .45);
-	margin-bottom: 16px;
-}
 
-button {
-	margin-top: 20px;
-}
-.pear-btn {
-	display: inline-block;
-	line-height: 38px;
-	white-space: nowrap;
-	cursor: pointer;
-	text-align: center;
-	box-sizing: border-box;
-	outline: none;
-	transition: 0.1s;
-	font-weight: 500;
-	padding: 0 18px;
-	height: 38px;
-	font-size: 14px;
-	color: #2f495e;
-	background-color: #edf2f7;
-	border-radius: 4px;
-	border: none;
-	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
-}
-
-.pear-btn i {
-	font-size: 13px;
-}
-
-.pear-btn:hover {
-	opacity: .8;
-	filter: alpha(opacity=80);
-	color: #409eff;
-	border-color: #c6e2ff;
-	background-color: #ECF5FF;
-}
-
-.pear-btn-primary {
-	background-color:#5FB878!important;
-	border: #2D8CF0;
-}
         </style>
 	</head>
 	<body>
-		<div class="content" style="top: 30%;">
-			<div class="content-r">
-				<h1>Pear Admin Think</h1>
-                <div></div>
-				<p>欢迎使用</p>
-				<button class="pear-btn pear-btn-primary" style=" "><a href ="<?=__url('login')?>">登录-</a></button>
-			</div>
+    
+<div class="layui-header" style ="background-color:#5FB878;padding:1em;">
+    <div>
+        <h1>DuckAdmin</h1><span> DuckPhp 的演示</span>
+    </div>
+</div>
+  <div class="layui-container">
+<!-- -->
+
+    <div class="layui-row " style="border:1px solid;">
+		<div class="layui-col-md8" >
+<!-- -->
+<pre>
+这里是更多说明
+目前系统运行方式为
+[V] 直接运行
+[X] 插件模式运行
+
+请注意 DuckAdmin 是 Composer Library 。而不是 Composer Project 。
+简而言之，请勿在 DuckAdmin 作为你的桩代码来修改，而是在你的工程里引入。
+
+DuckAdmin 是用来给 DuckPhp 项目做二次开发用的。
+
+你应该在这
+```php
+$options['ext'][\DuckAdmin\App\App::class] = true;
+
+```
+
+安装要点：
+
+数据库前缀。
+
+
+使用要点
+
+你后台的控制器类， 
+extends \DuckAdmin\Controller\BaseController
+
+
+或者在你的控制器类构造函数里加
+
+DuckAdmin\App\ControllerHelper::checkPermission(); 检查权限
+
+
+魔改 DuckAdmin
+
+最必须的魔改： 这个首页要替换
+在你的
+建立了：
+
+view\DuckAdimin\index.php 
+
+这个文件，重新设立你的入口页面。
+
+
+
+</pre>
+<!-- -->
+
 		</div>
+		<div class="layui-col-md4">
+        <div style="padding:1em;">
+		<form method="post" class="layui-form" style="border:1px solid; margin:1em;">
+            <div class="title" style="font-size:big;background-color:#5FB878;"><h1>登录</h1></div>
+            <div style="padding:1em;">
+			<div class="layui-form-item">
+				<input placeholder="账 户" type="text" name="username" hover class="layui-input"/>
+			</div>
+			<div class="layui-form-item">
+				<input placeholder="密 码" type="password" name="password"  hover class="layui-input"/>
+            </div>
+            <div class="layui-form-item">
+				<input placeholder="验证码 : " type="text" maxlength="4" name="captcha" hover class="code layui-input layui-input-inline"/>
+				<img id="codeimg" class="codeImage" />
+            </div>
+            <div class="layui-form-item">
+				<input type="checkbox" name="remember" title="30天内自动登录" lay-skin="primary" checked>
+			</div>
+			<div class="layui-form-item">
+				<button class="login" lay-submit lay-filter="login">
+					登 入
+				</button>
+			</div>
+            </div
+		</form>
+        </div>
+        </div>
+    </div>
+<!-- -->
+  </div>
 	</body>
 </html>
