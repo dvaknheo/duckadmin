21ec221dfa28eb0bb6b6c58365332cd72617444f
dvaknheo <85811616@qq.com>
2021-06-02 11:08:34 +0800

    保存

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -10,7 +10,6 @@ use DuckAdmin\App\BaseController as DuckAdminController;
 use DuckAdmin\App\BaseController as C;
 use DuckAdmin\Service\SessionService;
 use DuckAdmin\Service\AdminService;
-use DuckPhp\Core\App;
 // 我们这里只是偷懒一下啦。 等价于 ControllerHelper
 //这里和业务相关，严格禁止第三方 
 class BaseController extends DuckAdminController
@@ -18,14 +17,9 @@ class BaseController extends DuckAdminController
     public function __construct()
     {
         // 我们弄个小技巧，不允许直接访问，但我们可以创建一个实例填充，
-        if (self::class === App::Route()->getRouteCallingClass()) {
-            //__var_dump(App::Route()->getRouteCallingClass());
-            //禁止直接访问
-            C::Exit404();
-            return;
-        }
-        if(self::class === static::class){
-            // 作为助手模式。 
+        // 这里封装入一个静态方法里。
+        
+        if (DuckAdminController::CheckLocalController(self::class, static::class)) {
             return;
         }
         $this->initialize();
@@ -48,33 +42,4 @@ class BaseController extends DuckAdminController
         ///////////////// 正常流程
         $this->initViewData($admin, $path_info);
     }
-    protected function initViewData($admin, $path_info)
-    {
-        $menu = AdminService::G()->getMenu($admin['id'],$path_info);
-        C::assignViewData('menu', $menu);
-        C::assignViewData('admin', $admin);
-        C::setViewHeadFoot('header','footer');
-    }
-    //////////////////
-/*
-    public static function CheckPerMission()
-    {
-        $admin = SessionService::G()->getCurrentAdmin();
-        $flag = AdminService::G()->checkPermission($admin,$path_info);
-        
-        return $flag;
-    }
-    protected function CheckCapthca($captcha)
-    {
-        $builder = new CaptchaBuilder();
-        $flag = PhraseBuilder::comparePhrases($_SESSION['phrase']??'', captcha);
-    }
-    public function CaptchaBuilder()
-    {
-        $phraseBuilder = new PhraseBuilder(4, '0123456789');
-        $builder = new CaptchaBuilder(null, $phraseBuilder);
-        $builder->build()->output();        
-        $_SESSION['phrase'] = $builder->getPhrase();
-    }
-*/
 }
