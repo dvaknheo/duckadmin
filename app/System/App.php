61ed895f525265a57b1ef3cab1401396952133fc
dvaknheo <85811616@qq.com>
2021-09-13 16:02:02 +0800

    保存

--- app/System/App.php
+++ app/System/App.php
@@ -7,7 +7,6 @@
 namespace DuckAdminDemo\System;
 
 use DuckPhp\DuckPhp;
-//use DuckPhp\Component\AppPluginTrait;
 
 class App extends DuckPhp
 {
@@ -15,9 +14,13 @@ class App extends DuckPhp
     public $options = [
         //'path_info_compact_enable' => false,        
     ];
-    public function __construct()
+    public function onBeforeRun()
     {
-        parent::__construct();
+        //$this->
+    }
+    protected function checkInstall()
+    {
+        //
     }
     /**
      * 快速开始
@@ -25,10 +28,52 @@ class App extends DuckPhp
     public function command_go()
     {
         // 检查有没有设置，如果没有那么那么进入安装模式
-        //
+        //if(!(static::Setting('database') || static::Setting('database_list')){
+            //$this->doDatabaseSetting();
+        //}
         
-        // 然后进入 workerman 的项目启动
         
-        //
+    }
+    protected function runHttpServer()
+    {
+        $options = static::Parameters();
+        $options['path'] = $this->options['path'];
+        if (!empty($options['http_server'])) {
+            $class = str_replace('/', '\\', $options['http_server']);
+            HttpServer::G($class::G());
+        }
+        HttpServer::RunQuickly($options);
+    }
+    protected function doDatabaseSetting()
+    {
+        $desc = <<<EOT
+Configure Database(Mysql)
+
+database host[{host}]
+database port[{port}]
+database dbname[{dbname}]
+database username[{username}]
+database password[{password}]
+
+done;
+
+EOT;
+        $options=[
+            'host'=>'127.0.0.1',
+            'port'=>'3306',
+        ];        
+        $database_setting = ConsoleParent::G()->readLines($options,$desc);
+        $setting = static::Setting();
+        
+/*
+    'database_list' => [
+        [
+        'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=duckadmin;charset=utf8mb4;',
+        'username' => 'admin',
+        'password' => '123456',
+        'driver_options' => [],
+        ],
+    ],
+//*/
     }
 }
