864ce8bd0a26faa8cfc2c05dac32264c22599ed2
dvaknheo <85811616@qq.com>
2023-04-25 09:20:22 +0800

    移动 public 目录，防止误解

--- demo/public/index.php
+++ demo/public/index.php
@@ -4,11 +4,11 @@
  * From this time, you never be alone~
  */
  
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__.'/../../vendor/autoload.php');
 
 ////[[[[
 // 这里用本地的最新版本 DuckPhp 方便测试可调。
-if(is_file(__DIR__.'/../../DNMVCS/autoload.php')){
+if(is_file(__DIR__.'/../../../DNMVCS/autoload.php')){
     $funcs = spl_autoload_functions();
     $t =$funcs[0];
     spl_autoload_unregister($t);
