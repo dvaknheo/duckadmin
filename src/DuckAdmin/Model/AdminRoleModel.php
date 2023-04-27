22a96cba0d80e4bad8cf9305a0a03359526229df
dvaknheo <85811616@qq.com>
2023-04-27 13:41:10 +0800

    保存

---
+++ src/DuckAdmin/Model/AdminRoleModel.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPhp
+ * From this time, you never be alone~
+ */
+
+namespace DuckAdmin\Model;
+/**
+ * 菜单模型
+ */
+class OptionModel extends BaseModel
+{
+    public function foo()
+    {
+    }
+}
