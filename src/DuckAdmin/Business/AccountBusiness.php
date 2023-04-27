e3f00eff08aef38498049ac7ea3748e93453b7b4
dvaknheo <85811616@qq.com>
2023-04-27 13:40:06 +0800

    保存

--- src/DuckAdmin/Business/AccountBusiness.php
+++ src/DuckAdmin/Business/AccountBusiness.php
@@ -10,5 +10,21 @@ class AccountBusiness extends BaseBusiness
 	{
 		$data = json_decode(file_get_contents(__DIR__.'/data/account.json'),true);
 		return $data;
+		
+		$admin = admin();
+        if (!$admin) {
+            return $this->json(1);
+        }
+        $info = [
+            'id' => $admin['id'],
+            'username' => $admin['username'],
+            'nickname' => $admin['nickname'],
+            'avatar' => $admin['avatar'],
+            'email' => $admin['email'],
+            'mobile' => $admin['mobile'],
+            'isSupperAdmin' => Auth::isSupperAdmin(),
+            'token' => $request->sessionId(),
+        ];
+        return $this->json(0, 'ok', $info);
 	}
 }
--- src/DuckAdmin/Business/RuleBusiness.php
+++ src/DuckAdmin/Business/RuleBusiness.php
@@ -10,5 +10,95 @@ class RuleBusiness extends BaseBusiness
 	{
 		$data = json_decode(file_get_contents(__DIR__.'/data/rule.json'),true);
 		return $data;
+		
+		$rules = $this->getRules(admin('roles'));
+        $types = $request->get('type', '0,1');
+        $types = is_string($types) ? explode(',', $types) : [0, 1];
+        $items = Rule::orderBy('weight', 'desc')->get()->toArray();
+
+        $formatted_items = [];
+        foreach ($items as $item) {
+            $item['pid'] = (int)$item['pid'];
+            $item['name'] = $item['title'];
+            $item['value'] = $item['id'];
+            $item['icon'] = $item['icon'] ? "layui-icon {$item['icon']}" : '';
+            $formatted_items[] = $item;
+        }
+
+        $tree = new Tree($formatted_items);
+        $tree_items = $tree->getTree();
+        // 超级管理员权限为 *
+        if (!in_array('*', $rules)) {
+            $this->removeNotContain($tree_items, 'id', $rules);
+        }
+        $this->removeNotContain($tree_items, 'type', $types);
+        return $this->json(0, 'ok', Tree::arrayValues($tree_items));
 	}
+    /**
+     * 获取权限规则
+     * @param $roles
+     * @return array
+     */
+    protected function getRules($roles): array
+    {
+        $rules_strings = $roles ? Role::whereIn('id', $roles)->pluck('rules') : []; //model
+        $rules = [];
+        foreach ($rules_strings as $rule_string) {
+            if (!$rule_string) {
+                continue;
+            }
+            $rules = array_merge($rules, explode(',', $rule_string));
+        }
+        return $rules;
+    }
+///////////////////////////////////
+    /**
+     * 移除不包含某些数据的数组
+     * @param $array
+     * @param $key
+     * @param $values
+     * @return void
+     */
+    protected function removeNotContain(&$array, $key, $values)
+    {
+        foreach ($array as $k => &$item) {
+            if (!is_array($item)) {
+                continue;
+            }
+            if (!$this->arrayContain($item, $key, $values)) {
+                unset($array[$k]);
+            } else {
+                if (!isset($item['children'])) {
+                    continue;
+                }
+                $this->removeNotContain($item['children'], $key, $values);
+            }
+        }
+    }
+
+    /**
+     * 判断数组是否包含某些数据
+     * @param $array
+     * @param $key
+     * @param $values
+     * @return bool
+     */
+    protected function arrayContain(&$array, $key, $values): bool
+    {
+        if (!is_array($array)) {
+            return false;
+        }
+        if (isset($array[$key]) && in_array($array[$key], $values)) {
+            return true;
+        }
+        if (!isset($array['children'])) {
+            return false;
+        }
+        foreach ($array['children'] as $item) {
+            if ($this->arrayContain($item, $key, $values)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
