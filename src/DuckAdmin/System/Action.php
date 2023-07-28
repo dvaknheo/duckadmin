2e1f97bc0582be832b0e6cf552c27632bde1888a
dvaknheo <85811616@qq.com>
2023-07-28 21:43:56 +0800

    一些改动

---
+++ src/DuckAdmin/System/Action.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPhp
+ * From this time, you never be alone~
+ */
+
+namespace DuckAdmin\System;
+use DuckPhp\SingletonEx\SingletonExTrait;
+
+/**
+ * 你的 Contoller 控制器调用这里的静态方法类。
+ * 如果没法满足你的需求，那么请深入相关类
+ */
+class Action
+{
+    use SingletonExTrait;
+	public function getCurrentAdminId()
+	{
+		//
+	}
+	public function isSuperAdmin($admin_id)
+	{
+		//
+	}
+	public function hasPermission()
+	{
+		//
+	}
+    /**
+     * 检查权限 你的方法调用这个杜绝外部访问。
+     */
+    public function checkPermission()
+    {
+        //return AdminAction::G()->doCheckPermission();
+    }
+}
--- src/DuckAdmin/System/App.php
+++ src/DuckAdmin/System/App.php
@@ -22,10 +22,23 @@ class App extends DuckPhp
         'error_500' => '_sys/error_500',
         'controller_resource_prefix' => 'res/',  // 资源文件前缀
 		'controller_class_postfix' => 'Controller',
-
-        //////////////////
     ];
-    ///////
+    public function init()
+    {
+        parent::init();
+        
+        
+        return $this;
+    }
+    /////////
+    public static function Action()
+    {
+        return Action::G();
+    }
+    public static function Service()
+    {
+        return Service::G();
+    }
     ////////////// 命令行
     public function command_install()
     {
@@ -38,6 +51,24 @@ class App extends DuckPhp
         echo $this->install($parameters); // on InstallableTrait
         echo "Done \n";
     }
+	protected function switchDbManager()
+	{
+		$old = DbManager::G();
+		$options = $old->options;
+		if(empty($options['database']) || empty($options['database_list'])){
+			$connections = static::Config('connections',[],'database');
+			$post = $connections['mysql']??null;
+			if(!empty($post)){
+				DbManager::G()->options['database']=[
+					'dsn'=>"mysql:host={$post['host']};port={$post['port']};dbname={$post['database']};charset=utf8mb4;",
+					'username'=>$post['username'],	
+					'password'=>$post['password'],
+				];
+				DbManager::G()->init(DbManager::G()->options, DuckPhpCoreApp::G());
+			}
+		}
+	}
+
 }
 /////////////
 }
