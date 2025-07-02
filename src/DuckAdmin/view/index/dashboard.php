265b25578f236811759ec5bdea6a1cbb6f07810d
cgb <xx@xx.com>
2025-07-02 15:03:11 +0800

    调整一下系统的view

--- src/DuckAdmin/view/index/dashboard.php
+++ src/DuckAdmin/view/index/dashboard.php
@@ -7,7 +7,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" href="<?=__res('component/pear/css/pear.css')?>" />
-		<link rel="stylesheet" href="<?=__res('demos/css/console1.css')?>" />
 	</head>
 	<body class="pear-container">
 		<div>
